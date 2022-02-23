0ef353d021f099e6a98e260a77f7f786255437dc
Benjamin Fischer <c4spar@gmx.de>
2022-02-24 00:48:11 +0100

    chore(upgrade): nano_deploy v0.1.1

--- deps.ts
+++ deps.ts
@@ -17,6 +17,6 @@ export {
   SourceFile,
   styles,
   tw,
-} from "https://raw.githubusercontent.com/c4spar/nano-deploy/v0.1.0/mod.ts";
+} from "https://raw.githubusercontent.com/c4spar/nano-deploy/v0.1.1/mod.ts";
 
 export { assert } from "https://deno.land/std@0.121.0/testing/asserts.ts";
