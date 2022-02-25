d7cdd4e9e58806107b86ababfe5917c14032f35f
Benjamin Fischer <c4spar@gmx.de>
2022-02-25 01:16:16 +0100

    refactor: update src repository

--- main.ts
+++ main.ts
@@ -1,14 +1,12 @@
 import { ExamplesDataProvider, serve } from "./mod.tsx";
 
-if (import.meta.main) {
-  await serve({
-    src: [{ src: "c4spar/deno-cliffy@main:docs/src", prefix: "/docs" }],
-    providers: [{
-      component: ExamplesDataProvider,
-      props: {
-        src: "c4spar/deno-cliffy@main:docs/examples",
-        selected: "command.ts",
-      },
-    }],
-  });
-}
+await serve({
+  src: [{ src: "c4spar/cliffy-docs@main:src", prefix: "/docs" }],
+  providers: [{
+    component: ExamplesDataProvider,
+    props: {
+      src: "c4spar/deno-cliffy@main:docs/examples",
+      selected: "command.ts",
+    },
+  }],
+});
