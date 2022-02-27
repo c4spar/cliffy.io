917ca0f92a8f1ee75ca17b44dbd01b593b3a82af
Benjamin Fischer <c4spar@gmx.de>
2022-02-27 01:00:44 +0100

    refactor: refactor main file

--- main.ts
+++ main.ts
@@ -1,13 +1,65 @@
-import { ExamplesDataProvider, serve } from "./mod.tsx";
+import {
+  serve,
+  SourceFile,
+} from "./deps.ts";
+import { ExamplesDataProvider } from "./pages/index.tsx";
 
 await serve({
+  name: "Cliffy",
   repository: "c4spar/deno-cliffy",
-  src: [{ src: "c4spar/cliffy-docs@main:src", prefix: "/docs" }],
+  src: [
+    "pages",
+    { src: "c4spar/cliffy-manual@main:src", prefix: "/docs" },
+  ],
   providers: [{
     component: ExamplesDataProvider,
     props: {
-      src: "c4spar/deno-cliffy@main:docs/examples",
+      src: "examples",
       selected: "command.ts",
     },
   }],
+  pages: true,
+  nav: {
+    collapse: true,
+    items: [{
+      label: "API",
+      href: "https://doc.deno.land/https://deno.land/x/cliffy@{rev}/mod.ts",
+    }],
+  },
+  theme: {
+    fontFamily: {
+      display: ["Fredoka One"],
+    },
+  },
+  scripts: {
+    "/google-fonts.css": {
+      url:
+        "https://fonts.googleapis.com/css2?family=Fredoka+One&display=swap",
+      contentType: "text/css",
+    },
+  },
+  sanitize(file: SourceFile) {
+    // Add selected version to cliffy module imports.
+    if (file.rev) {
+      return file.content
+        .replace(
+          /https:\/\/deno\.land\/x\/cliffy\//g,
+          `https://deno.land/x/cliffy@${file.rev}/`,
+        )
+        .replace(
+          /https:\/\/deno\.land\/x\/cliffy@<version>\//g,
+          `https://deno.land/x/cliffy@${file.rev}/`,
+        )
+        .replace(
+          /https:\/\/x\.nest\.land\/cliffy@<version>\//g,
+          `https://x.nest.land/cliffy@${file.rev}/`,
+        )
+        .replace(
+          /https:\/\/raw\.githubusercontent\.com\/c4spar\/deno-cliffy\/<version>\//g,
+          `https://raw.githubusercontent.com/c4spar/deno-cliffy/${file.rev}/`,
+        );
+    }
+
+    return file.content;
+  },
 });
