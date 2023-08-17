9bf480851667a064005dc54c6160ade3a367ad35
Benjamin Fischer <c4spar@gmx.de>
2023-08-17 20:14:51 +0200

    update night-owl

--- deps.ts
+++ deps.ts
@@ -18,7 +18,7 @@ export {
   SourceFile,
   styles,
   tw,
-} from "https://raw.githubusercontent.com/c4spar/night-owl/1f428bde452f8551aa912c5be2eebc25b99293be/mod.ts";
-export { env } from "https://raw.githubusercontent.com/c4spar/night-owl/1f428bde452f8551aa912c5be2eebc25b99293be/lib/utils.ts";
+} from "https://raw.githubusercontent.com/c4spar/night-owl/1a7bf2b2115b2b66b9ea8361bd8b5fa3de807ad9/mod.ts";
+export { env } from "https://raw.githubusercontent.com/c4spar/night-owl/1a7bf2b2115b2b66b9ea8361bd8b5fa3de807ad9/lib/utils.ts";
 
 export { assert } from "https://deno.land/std@0.182.0/testing/asserts.ts";
